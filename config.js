e916ba099c692e2b5198d4d56903b80218b86e72
conaynay <calcon007@gmail.com>
2018-04-09 23:39:08 -0700

    revert

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://thinkful-connie:password123@ds241019.mlab.com:41019/blog-app-mongoose-challenge';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app';
 exports.PORT = process.env.PORT || 8080;
